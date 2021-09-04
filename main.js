53c95bb70e280cbd22acf8a9721ee423d806f52a
Alp Güler <alpguler.business@gmail.com>
2021-09-04 13:00:05 +0300

    added furry styles and variants

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             image: './assets/images/kole1.jpg',
             url: 'https://alpguler.gq/onlyfans.html',
             available: 0,
-            discount: true
+            discount: true,
+            experiences: ['8 yıl tecrübe', 'İtalyan ve Türk Mutfağı', 'Türkçe, Almanca ve Azerice konuşabilme', 'Tam 200 cümle kurabiliyor'],
+            variants: [
+                { id: 2234, age: '23' },
+                { id: 2235, age: '21' }
+              ],
+            furryStyles: ['Neko 🐈', 'Kitsune 🦊', 'Canine 🐕', 'Dragon 🐉'],
         }
     }
 })
